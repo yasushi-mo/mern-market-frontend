9f9532836ef60afa8c4e73870bafd6d80fd84721
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-04-30 08:47:36 +0900

    Add: User Register function

--- src/pages/user/Register.js
+++ src/pages/user/Register.js
@@ -1,7 +1,63 @@
+import { useState } from "react";
+
 export const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:5000/user/register", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+      });
+      const jsonResponse = await response.json();
+      alert(jsonResponse.message);
+    } catch (error) {
+      alert("ユーザー登録失敗");
+    }
+  };
+
   return (
     <div>
-      <h1>登録ページ</h1>
+      <h1>ユーザー登録ページ</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          type="text"
+          name="name"
+          placeholder="名前"
+          required
+        />
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          type="text"
+          name="email"
+          placeholder="メールアドレス"
+          required
+        />
+        <input
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          type="text"
+          name="password"
+          placeholder="パスワード"
+          required
+        />
+        <button>登録</button>
+      </form>
     </div>
   );
 };
