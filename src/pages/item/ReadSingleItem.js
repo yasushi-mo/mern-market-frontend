b4a93599f3f3f60005c9c99894869d3e4098dff5
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-05-01 08:57:51 +0900

    Add: Read Single Item

--- src/pages/item/ReadSingleItem.js
+++ src/pages/item/ReadSingleItem.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
 export const ReadSingleItem = () => {
+  const params = useParams();
+
+  const [item, setItem] = useState({
+    title: "",
+    price: "",
+    image: "",
+    description: "",
+  });
+
+  const getSingleItem = async () => {
+    const response = await fetch(`http://localhost:5000/item/${params.id}`);
+    const jsonResponse = await response.json();
+    setItem(jsonResponse.singleItem);
+  };
+
   return (
     <div>
       <h1>1つのアイテムデータ</h1>
+      <div>
+        {item.image && (
+          <img src={require(`../../images/${item.image}.jpg`)} alt="item" />
+        )}
+      </div>
+      <div>
+        <h1>{item.title}</h1>
+        <h2>¥{item.price}</h2>
+        <hr />
+        <p>{item.description}</p>
+      </div>
+      <button onClick={getSingleItem}>データ取得</button>
     </div>
   );
 };
