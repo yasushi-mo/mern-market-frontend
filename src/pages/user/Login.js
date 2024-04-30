95f10561ec04225bdcf229966e5b7672d2495a8e
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-04-30 09:37:37 +0900

    Add: Login function

--- src/pages/user/Login.js
+++ src/pages/user/Login.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:5000/user/login", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      const jsonResponse = await response.json();
+      localStorage.setItem("token", jsonResponse.token);
+      alert(jsonResponse.message);
+    } catch (error) {
+      alert("ログイン失敗");
+    }
+  };
+
   return (
     <div>
-      <h1>ログインページ</h1>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          type="text"
+          name="email"
+          placeholder="メールアドレス"
+          required
+        />
+        <input
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          type="text"
+          name="password"
+          placeholder="パスワード"
+          required
+        />
+        <button>ログイン</button>
+      </form>
     </div>
   );
 };
