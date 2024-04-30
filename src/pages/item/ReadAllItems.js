e84d2154468f28645f0bf40895b371de1ca20313
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-04-30 10:40:40 +0900

    Add: Read All Items

--- src/pages/item/ReadAllItems.js
+++ src/pages/item/ReadAllItems.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 export const ReadAllItems = () => {
+  const [allItems, setAllItems] = useState();
+
+  const getAllItems = async () => {
+    try {
+      const response = await fetch("http://localhost:5000");
+      const jsonResponse = await response.json();
+      setAllItems(jsonResponse);
+    } catch (error) {}
+  };
+
   return (
     <div>
       <h1>すべてのアイテムデータ</h1>
+      {allItems &&
+        allItems.allItems.map((item) => (
+          <div key={item._id}>
+            <img src={require(`../../images/${item.image}.jpg`)} alt="item" />
+            <h2>{item.price}</h2>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      <button onClick={getAllItems}>全データ取得</button>
     </div>
   );
 };
