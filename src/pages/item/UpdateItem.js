e5e1f26a29572315a2ec595be7f243c4077f58e7
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-05-02 10:41:42 +0900

    Add: Update item

--- src/pages/item/UpdateItem.js
+++ src/pages/item/UpdateItem.js
@@ -1,7 +1,65 @@
+import { useEffect, useState } from "react";
+import { ItemForm } from "../../components/ItemForm";
+import { useParams } from "react-router-dom";
+
 export const UpdateItem = () => {
+  const params = useParams();
+
+  const [item, setItem] = useState({
+    title: "",
+    image: "",
+    price: "",
+    description: "",
+  });
+
+  const handleChange = (event) => {
+    setItem({
+      ...item,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(
+        `http://localhost:5000/item/update/${params.id}`,
+        {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            authorization: localStorage.getItem("token"),
+          },
+          body: JSON.stringify(item),
+        }
+      );
+      const jsonResponse = await response.json();
+      alert(jsonResponse.message);
+    } catch (error) {
+      alert("アイテム編集失敗");
+    }
+  };
+
+  useEffect(() => {
+    const getSingleItem = async () => {
+      const response = await fetch(`http://localhost:5000/item/${params.id}`);
+      const jsonResponse = await response.json();
+      setItem(jsonResponse.singleItem);
+    };
+    getSingleItem();
+  }, [params.id]);
+
   return (
     <div>
-      <h1>アイテム更新</h1>
+      <h1>アイテム編集</h1>
+      <ItemForm
+        item={item}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        buttonLabel="編集"
+      />
     </div>
   );
 };
